de0b3e210c0a2317fa67fcfaf6ac786a4c99a18a
surayac <surayacsm@gmail.com>
2025-11-27 15:05:35 +0100

    fix: edit video entity to add manyToOne relation to axis

--- src/main/java/com/laquintapata/app/entity/Video.java
+++ src/main/java/com/laquintapata/app/entity/Video.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,13 +27,17 @@ public class Video {
     private Long id;
 
     @Column(nullable = false)
-    private String titulo;
+    private String title;
 
-    private String descripcion;
+    private String description;
 
     @Column(nullable = false)
     private String url;
 
     @Column(nullable = false)
     private String thumbnailUrl;   
+
+    @ManyToOne
+    @JoinColumn(name = "axis_id", nullable = false)
+    private Axis axis;
 }
