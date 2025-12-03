affbe42b59c9fb01b3c936c929a8747b4faa2493
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2025-12-03 14:27:14 +0100

    feat:Migrant.java added to entity

---
+++ src/main/java/com/laquintapata/app/entity/Migrant.java
@@ -0,0 +1,28 @@
+package com.laquintapata.app.entity;
+
+import java.util.UUID;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "migrants")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Migrant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "last_name", nullable = false, length = 100)
+    private String lastName;
+
+    @Column(name = "origin_id", nullable = false)
+    private Integer originId;
+}
