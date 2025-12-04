088a525715afd29b4f9739ddb27a7710a4dceba0
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2025-12-04 08:57:17 +0100

    feaure:Added migrantMapper.java file

---
+++ src/main/java/com/laquintapata/app/mapper/MigrantMapper.java
@@ -0,0 +1,16 @@
+package com.laquintapata.app.mapper;
+
+import com.laquintapata.app.dto.request.MigrantRequest;
+import com.laquintapata.app.dto.response.MigrantResponse;
+import com.laquintapata.app.entity.Migrant;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface MigrantMapper {
+
+    MigrantMapper INSTANCE = org.mapstruct.factory.Mappers.getMapper(MigrantMapper.class);
+
+    MigrantResponse migrantToMigrantResponse(Migrant migrant);
+
+}
